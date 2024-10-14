d4140bdeca08e59bbfa8261e6407c275edbd873c
bruce mckenzie <bjmckenz@gmail.com>
2024-10-14 09:53:28 -0700

    reformatted

--- p1_environment.c
+++ p1_environment.c
@@ -25,8 +25,7 @@ void add_env_var()
 {
     char buf[100];
     sprintf(buf, "NEWVAR_%d", getpid());
-    setenv(buf, "hello",
-        ADD_OR_OVERWRITE_ENV_VAR);
+    setenv(buf, "hello", ADD_OR_OVERWRITE_ENV_VAR);
 }
 
 // OLD, not portable, never do this.
@@ -43,11 +42,9 @@ int main(int argc, char* argv[])
              "NEWVAR_<pid>");
 
     if (getenv("TEST_ME") == NULL) {
-        printf(
-            "********* TEST_ME not "
-            "found; ADDED '=HELLO'\n");
-        setenv("TEST_ME", "hello",
-            ADD_BUT_DO_NOT_OVERWRITE_VAR);
+        printf("********* TEST_ME not "
+               "found; ADDED '=HELLO'\n");
+        setenv("TEST_ME", "hello", ADD_BUT_DO_NOT_OVERWRITE_VAR);
     } else {
         printf("********* TEST_ME "
                "found; removed\n");
@@ -57,13 +54,11 @@ int main(int argc, char* argv[])
     fflush(stdout);
     if (fork() == 0) {
         add_env_var();
-        printenv(
-            "CHILD after NEWVAR_<pid>");
+        printenv("CHILD after NEWVAR_<pid>");
         exit(0);
     }
 
     wait(NULL);
-    printenv(
-        "PARENT AFTER CHILD EXITS");
+    printenv("PARENT AFTER CHILD EXITS");
     return 0;
 }
