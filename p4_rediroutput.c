3a26678b1d980fe223fccab76e76f95994a138ce
bruce mckenzie <bjmckenz@gmail.com>
2024-10-13 23:00:15 -0700

    Pipeing for fun

--- p4_rediroutput.c
+++ p4_rediroutput.c
@@ -1,33 +1,43 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+    ./this <outputFile> <command> <arg1> <arg2> ...
+
+    <command> MUST BE absolute path
+*/
+
 int main(int argc, char* argv[])
 {
-    // But to use execve, we need a NULL
-    // after that, so we WOULD add one
-    // BUT we're going to skip "us"
-    // (argv[0], our executable) So we
-    // allocate the same size.
-    char** newargv = (char**)malloc(
-        sizeof(char*) * argc);
-
-    // we "copy it down" by one
-    for (int ix = 1; ix < argc; ix++) {
-        newargv[ix - 1]
-            = (char*)argv[ix];
+    if (argc < 3) {
+        fprintf(stderr,
+            "Usage: %s <outputFile> <command> [<arg1> <arg2> ...]\n", argv[0]);
+        return 1;
     }
-    newargv[argc - 1] = NULL;
+
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        fprintf(stderr, "Failed to open %s\n", argv[1]);
+        return 1;
+    }
+
+    // add one for NULL, -2 cause we're skipping the first two
+    char** newargv = (char**)malloc(sizeof(char*) * (1 + argc - 2));
+
+    // we "copy it down" by two
+    for (int ix = 2; ix < argc; ix++) {
+        newargv[ix - 2] = (char*)argv[ix];
+    }
+    newargv[argc - 2] = NULL;
 
     int child_pid = fork();
     if (child_pid == 0) {
-        // assumes that the first param
-        // is a absolute path to the
-        // executable
-        execve(
-            newargv[0], newargv, NULL);
+        dup2(fd, STDOUT_FILENO); /* STDOUT_FILENO = 1 */
+        close(fd);
 
-        // no need to exit!
+        execve(newargv[0], newargv, NULL);
     }
 
     printf("%s pid is %d. forked %d. "
---
+++ p5_pipeit.c
@@ -0,0 +1,92 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*
+    ./this <command1> <arg1> <arg2> ... /// <command2> <arg1> <arg2> ...
+
+    <command>s MUST BE absolute path
+*/
+
+#define COMMAND_SEPARATOR "///"
+
+#define READ_SIDE 0
+#define WRITE_SIDE 1
+
+// adds NULL at the end
+void copy_ptrs_from_to(char** to, char** from, int from_ix, int to_ix)
+{
+    for (int dest_ix = 0; dest_ix < to_ix - from_ix + 1; dest_ix++)
+        to[dest_ix] = from[from_ix + dest_ix];
+    to[to_ix - from_ix + 1] = NULL;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 4) {
+        fprintf(stderr,
+            "Usage: %s <command1> [<arg1> <arg2> ...] /// <command2> [<arg1> "
+            "<arg2> ...] \n",
+            argv[0]);
+        return 1;
+    }
+
+    int commands_separator_ix;
+
+    for (commands_separator_ix = 0;
+        commands_separator_ix < argc
+        && strcmp(argv[commands_separator_ix], COMMAND_SEPARATOR);
+        commands_separator_ix++)
+        ;
+
+    if (commands_separator_ix == argc) {
+        fprintf(
+            stderr, "Command separator '%s' not found\n", COMMAND_SEPARATOR);
+        return 1;
+    }
+
+    // +1 for NULL
+    char** first_cmd = (char**)malloc(sizeof(char*) * argc + 1);
+    char** second_cmd = (char**)malloc(sizeof(char*) * argc + 1);
+
+    copy_ptrs_from_to(first_cmd,  argv, 1, commands_separator_ix - 1);
+    copy_ptrs_from_to(second_cmd, argv, commands_separator_ix + 1, argc - 1);
+
+
+    // creates a pair of pipes: write to pipe_from_read_to_write[1], read from pipe_from_read_to_write[0]
+    int pipe_from_read_to_write[2];
+    pipe(pipe_from_read_to_write);
+
+    int command1_pid = fork();
+
+    if (command1_pid == 0) {
+        // first command process
+        close(pipe_from_read_to_write[READ_SIDE]);
+        dup2(pipe_from_read_to_write[WRITE_SIDE], STDOUT_FILENO);
+
+        execve(first_cmd[0], first_cmd, NULL);
+
+        fprintf(stderr, "Failed to execute first: %s\n", first_cmd[0]);
+        // children should exit if exec fails
+        _exit(1);
+    }
+
+    int command2_pid = fork();
+    if (command2_pid == 0) {
+        // second command process
+        close(pipe_from_read_to_write[WRITE_SIDE]);
+        dup2(pipe_from_read_to_write[READ_SIDE], STDIN_FILENO);
+
+        execve(second_cmd[0], second_cmd, NULL);
+
+        fprintf(stderr, "Failed to execute second: %s\n", second_cmd[0]);
+        // children should exit if exec fails
+        _exit(1);
+    }
+
+    printf("parent %s pid is %d. forked %d and %d. Parent exiting\n",
+        argv[0], getpid(), command1_pid, command2_pid);
+    return 0;
+}
